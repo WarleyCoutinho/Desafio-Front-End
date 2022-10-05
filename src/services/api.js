1251c8089f765080e6e731cc9a7e1401a4541b33
Warley Coutinho <warleycoutinho@icloud.com>
2022-10-05 15:24:00 -0300

    atualizando as bibliotecas.

--- src/services/api.js
+++ src/services/api.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 // Variáveis de modo de produção
 
-const api = axios.create({
-  baseURL: 'https://tool-back-end.herokuapp.com',
-});
+// const api = axios.create({
+//   baseURL: 'https://tool-back-end.herokuapp.com',
+// });
 
 // Variáveis de modo de desenvolvimento
 
-// const api = axios.create({
-//   baseURL: 'http://localhost:3000',
-// });
+const api = axios.create({
+  baseURL: 'http://localhost:3000',
+});
 
 export default api;
