217bf7145fe74f18cf3f70f0a05f673fb7df6340
Warley Coutinho <warleycoutinho@icloud.com>
2022-10-05 14:55:36 -0300

    Atualizando as variáveis de modo de produção.

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 
+// Variáveis de modo de produção
+
 const api = axios.create({
-  baseURL: ' https://tool-back-end.herokuapp.com/',
+  baseURL: 'https://tool-back-end.herokuapp.com',
 });
+
+// Variáveis de modo de desenvolvimento
+
+// const api = axios.create({
+//   baseURL: 'http://localhost:3000',
+// });
 
 export default api;
